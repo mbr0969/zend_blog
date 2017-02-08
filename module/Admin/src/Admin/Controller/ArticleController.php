99fc15480d205a1ceff692815733dd1cfd71297e
papa <mbr0969@gmail.com>
2017-02-08 22:55:23 +0300

    08.02.17 22:54 добавление статьи окончание

--- module/Admin/src/Admin/Controller/ArticleController.php
+++ module/Admin/src/Admin/Controller/ArticleController.php
@@ -1,9 +1,12 @@
 <?php
 namespace Admin\Controller;
 use Application\Controller\BaseAdminController as BaseController;
+use Blog\Entity\Arcticle;
 use DoctrineORMModule\Paginator\Adapter\DoctrinePaginator as DoctrineAdapter;
 use Doctrine\ORM\Tools\Pagination\Paginator as ORMPaginator;
 use Zend\Paginator\Paginator;
+use Admin\Form\ArticleAddForm;
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydraror;
 
 
 class ArticleController  extends BaseController{
@@ -22,6 +25,46 @@ class ArticleController  extends BaseController{
     }
 
     public function addAction(){
+        $em= $this->entityManager();
+        $form = new ArticleAddForm($em);
+
+        $request = $this->getRequest();
+        if ($request->isPost()){
+            $message = $status ='';
+
+            $data = $request->getPost();
+
+            $article = new Arcticle();
+            $form->setHydrator(new DoctrineHydraror($em, '\Arcticle'));
+            $form->bind($article);
+            $form->setData($data);
+
+            if ($form->isValid()){
+
+                $em->persist($article);
+                $em->flush();
+
+                $status = 'success';
+                $message = 'Статья добавленна';
+            }else{
+                $status = 'error';
+                $message = 'Ошибка параметров';
+                foreach ($form->getInputFilter()->getInvalidInput() as $errors){
+                    foreach ($errors->getMessages() as $error){
+                        $message .=  ' '. $error;
+                    }
+                }
+            }
+        }else {
+            return array('form' => $form);
+        }
+
+        if ($message) {
+            $this->flashMessenger()
+                ->setNamespace($status)
+                ->addMessage($message);
+        }
+        return $this->redirect()->toRoute('admin/article');
 
     }
 
---
+++ module/Admin/src/Admin/Form/ArticleAddForm.php
@@ -0,0 +1,112 @@
+<?php
+namespace Admin\Form;
+
+use Zend\Form\Form;
+use Zend\Form\Element;
+
+use  DoctrineModule\Persistence\ObjectManagerAwareInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Admin\Filter\ArticleAddInputFilter;
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydraror;
+use Blog\Entity\Arcticle;
+
+class ArticleAddForm extends Form  implements ObjectManagerAwareInterface {
+
+    protected $objectManager;
+
+    public function __construct(ObjectManager $objectManager) {
+        parent::__construct('articleAddForm');
+        $this->setObjectManager($objectManager);
+        $this->createElements();
+    }
+
+    public function setObjectManager(ObjectManager $objectManager) {
+        $this ->objectManager = $objectManager;
+    }
+
+    public function getObjectManager() {
+        return $this ->objectManager;
+    }
+
+    public function createElements(){
+        $this->setAttribute('method','post');
+        $this->setAttribute('class','bs-example form-horizontal');
+
+        $this->setInputFilter(new ArticleAddInputFilter());
+
+        $this->add(array(
+                    'type' => 'DoctrineModule\Form\Element\ObjectSelect',
+                     'name' => 'category',
+                      'options' => array(
+                          'label' => 'Категория',
+                          'empty_option' => 'Выберете категорию...',
+                          'object_manager'=> $this->getObjectManager(),
+                          'target_class' => 'Blog\Entity\Category',
+                          'property'    => 'categoryName',
+                      ),
+                'attributes' => array(
+                    'class' => 'form-control',
+                    'required' => 'required',
+                ),
+            ));
+
+        $this->add(array(
+                'name' => 'title',
+                'type' => 'Text',
+                'options' => array(
+                    'min' => 3,
+                    'max' =>100,
+                    'label' => 'Заголовок',
+                ),
+                'attributes' => array(
+                    'class' => 'form-control',
+                    'required' => 'required',
+                ),
+            ));
+        $this->add(array(
+            'name' => 'shotArticle',
+            'type' => 'Textarea',
+            'options' => array(
+                  'label' => 'Начало статьи',
+                ),
+                'attributes' => array(
+                    'class' => 'form-control ckeditor',
+                ),
+            ));
+
+        $this->add(array(
+            'name' => 'arcticle',
+            'type' => 'Textarea',
+            'options' => array(
+                'label' => 'Текст статьи',
+            ),
+            'attributes' => array(
+                'class' => 'form-control ckeditor',
+                'required' => 'required',
+            ),
+        ));
+
+        $this->add( array(
+                'name' => 'isPublic',
+                'type' => 'CheckBox',
+                'options' => array(
+                 'label' => 'Опубликовать',
+                    'use_hidden_Element' => true,
+                    'checked_value' => 1,
+                    'unchecked_value' => 0
+                ),
+           ));
+        $this->add(array(
+            'name' =>'submit',
+            'type' => 'Submit',
+            'attributes' => array(
+                'value' => 'Сохранить',
+                'id' => 'btn_submit',
+                'class' => 'btn btn-primary',
+            ),
+        ));
+    }
+
+
+}
