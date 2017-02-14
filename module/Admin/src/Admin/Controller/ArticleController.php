d678ad325a570bf9a2184c7d7e8c862d16e7b546
papa <mbr0969@gmail.com>
2017-02-14 21:14:27 +0300

    14.02.17  удаление статьи статьи окончание с исправлениями

--- module/Admin/src/Admin/Controller/ArticleController.php
+++ module/Admin/src/Admin/Controller/ArticleController.php
@@ -69,4 +69,80 @@ class ArticleController  extends BaseController{
 
     }
 
+    public function editAction() {
+        $message = $status = '';
+        $em = $this->getEntityManager();
+        $form = new ArticleAddForm($em);
+
+        $id = (int)$this->params()->fromRoute('id', 0);
+        $article = $em->find('Blog\Entity\Arcticle', $id);
+
+
+        if (empty($article)){
+            $status = 'error';
+            $message = 'Статья не найдена';
+            $this->flashMessenger()
+                ->setNamespace($status)
+                ->addMessage($message);
+            return $this->redirect()->toRoute('admin/article');
+        }
+
+        $form->setHydrator(new DoctrineHydraror($em, '\Arcticle'));
+        $form->bind($article);
+
+        $request = $this->getRequest();
+        if ($request->isPost()){
+            $data = $request->getPost();
+            $form->setData($data);
+            if ($form->isValid()){
+                $em->persist($article);
+                $em->flush();
+
+                $status = 'success';
+                $message = 'Статья обновленна';
+            }else{
+                $status = 'error';
+                $message = 'Ошибка параметров';
+                foreach ($form->getInputFilter()->getInvalidInput() as $errors){
+                    foreach ($errors->getMessages() as $error){
+                        $message .=  ' '. $error;
+                    }
+                }
+            }
+
+        }else {
+            return array('form'=>$form, 'id'=>$id);
+        }
+        if ($message) {
+            $this->flashMessenger()
+                ->setNamespace($status)
+                ->addMessage($message);
+        }
+        return $this->redirect()->toRoute('admin/article');
+    }
+
+    public function deleteAction() {
+        $id = (int) $this->params()->fromRoute('id',0);
+        $em = $this->getEntityManager();
+
+        $status = 'success';
+        $message = 'Запись удалена';
+
+        try{
+            $repository = $em->getRepository('Blog\Entity\Arcticle');
+            $article = $repository->find($id);
+            $em->remove($article);
+            $em->flush();
+
+        }catch (\Exception $ex){
+            $status = 'error';
+            $message = 'Ошибка удаление записи: ' . $ex->getMessage();
+        }
+        $this->flashMessenger()
+            ->setNamespace($status)
+            ->addMessage($message);
+        return $this->redirect()->toRoute('admin/article');
+
+    }
+
 }
